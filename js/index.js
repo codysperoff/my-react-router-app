6324e55226287c726ddbd64f1d919adef3ca7cc5
Cody Speroff <cnsperoff@gmail.com>
2017-05-27 15:45:16 -0500

    created new react router with facebook incubator react app file

---
+++ js/index.js
@@ -0,0 +1,15 @@
+mport React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+
+import ContactListContainer from './components/contact-list-container';
+
+const routes = (
+    <Router history={hashHistory}>
+    <Route path="/contacts" component={ContactListContainer} />
+    </Router>
+);
+
+document.addEventListener('DOMContentLoaded', () =>
+                          ReactDOM.render(routes, document.getElementById('app'))
+                         );
